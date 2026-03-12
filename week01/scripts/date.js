b4746d8ae905b7ee791208d9855145cd484a0df2
mantech-set <kon3manbeats@gmail.com>
2026-03-12 16:39:26 +0100

    Delete date.js

--- week01/scripts/date.js
+++
@@ -1,7 +0,0 @@
-const year = document.querySelector("#year");
-const today = new Date();
-
-year.textContent = today.getFullYear();
-
-document.getElementById("lastModified").textContent =
-`Last Modified: ${document.lastModified}`;
